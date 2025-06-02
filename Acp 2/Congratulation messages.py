f1238ed3698114fe7b6b6a50f3f7e825e6be0a2e
Makima20 <cookiebrown638@gmail.com>
2025-06-02 22:33:47 +0600

    Congratulation message

---
+++ Acp 2/Congratulation messages.py
@@ -0,0 +1,13 @@
+name = input("Enter your name in UPPERCASE:")
+
+lower_name = name.lower()
+print("Lowercase:",lower_name)
+
+title_name = name.title()
+print("Title Case:", title_name)
+
+name_length = len(name)
+print("Length of your name:", name_length)
+
+is_alpha = name.isalpha()
+print("Is your name alphabetic?", is_alpha)
