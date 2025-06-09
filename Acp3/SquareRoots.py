4ca6084f357e88817299e9f9578b0f46b141abbc
Makima20 <cookiebrown638@gmail.com>
2025-06-09 20:21:25 +0600

    Square root

---
+++ Acp3/SquareRoots.py
@@ -0,0 +1,7 @@
+num = int(input("Enter any number: "))
+float(num)
+square_root = num ** 0.5
+
+
+print("The square root of", num, "is", square_root)
+
---
+++ Conditional Statement/Index.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number:  "))
+if num > 0:
+    print("Number is a positive number")
+else:
+    print("Number is a negative number")
